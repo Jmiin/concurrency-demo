097e6d35186c50ed0ecad82784d8cd7dc4656355
jiangmin <jmiin@qq.com>
2020-09-11 10:53:43 +0800

    [add]ThreadGroup和Thread的优先级

--- src/main/java/thread/group/ThreadDemo.java
+++ src/main/java/thread/group/ThreadDemo.java
@@ -21,5 +21,34 @@ public class ThreadDemo {
 
         System.out.println("main线程的线程组的名字为："
                         + Thread.currentThread().getThreadGroup().getName());
+        testUncatchException();
+        diffPriority();
+    }
+
+    public static void testUncatchException() {
+        ThreadGroup threadGroup1 = new ThreadGroup("group1") {
+            // 继承ThreadGroup并重新定义以下方法
+            // 在线程成员抛出unchecked exception
+            // 会执行此方法
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(t.getName() + ": " + e.getMessage());
+            }
+        };
+        // 这个线程是threadGroup1的⼀员
+        Thread thread1 = new Thread(threadGroup1, ()-> {
+                // 抛出unchecked异常
+                throw new RuntimeException("测试异常");
+            });
+        thread1.start();
+    }
+
+    public static void diffPriority() {
+        ThreadGroup threadGroup = new ThreadGroup("t1");
+        threadGroup.setMaxPriority(6);
+        Thread thread = new Thread(threadGroup, "thread");
+        thread.setPriority(9);
+        System.out.println("我是线程组的优先级" + threadGroup.getMaxPriority());
+        System.out.println("我是线程的优先级" + thread.getPriority());
     }
 }
